103eeb76a001b96853f15934a366d67f661a21bf
tianpusen <tps@jfpal.com>
2017-05-28 11:02:24 +0800

    spring-boot-hello-world restful返回json报文

---
+++ spring-boot-hello-world/README.md
@@ -0,0 +1,5 @@
+# spring 默认返回json格式
+  curl http://localhost:8080/greeting
+  curl http://localhost:8080/greeting?name='tianpusen'
+  curl http://localhost:8080/user/selectByUserID
+  curl http://localhost:8080/user/selectByUserID?userID=003
---
+++ spring-boot-hello-world/src/main/java/tian/pusen/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package tian.pusen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tian.pusen.entity.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    // curl http://localhost:8080/greeting
+    // curl http://localhost:8080/greeting?name='tianpusen'
+    @RequestMapping(value="/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
