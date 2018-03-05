41270c0ce16b01fbbeb8877ae564544a43b6702d
田圃森 <tps@jfpal.com>
2018-03-05 15:45:40 +0800

    spring-boot-web中增加循环输出

--- spring-boot-web/src/main/java/tian/pusen/web/GreetingController.java
+++ spring-boot-web/src/main/java/tian/pusen/web/GreetingController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+
 @Controller
 public class GreetingController {
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("emplyees", Arrays.asList(new String[]{"AB", "12", "X#"}));
         return "greeting";
     }
 
